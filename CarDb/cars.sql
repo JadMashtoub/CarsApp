a262db4b0484e5c31daf011a0b809c26fb58bf46
Jad Mashtoub <jadmashtoub1@gmail.com>
2025-01-16 21:33:26 +1100

    first commit

---
+++ CarDb/cars.sql
@@ -0,0 +1,21 @@
+
+IF OBJECT_ID('cars') IS NOT NULL
+DROP TABLE SchemaName.TableName
+GO
+CREATE TABLE cars
+(
+    -- carID int AUTO_INCREMENT PRIMARY KEY, --PK AUTOINCREMENT
+    [name] [NVARCHAR](50) NOT NULL,
+    model [NVARCHAR](50) NOT NULL,
+    hasPlates [NVARCHAR](50) NOT NULL, --Can actually be null
+    plateNo [NVARCHAR](50) NOT NULL,
+    keyNo [NVARCHAR](50) NOT NULL
+    PRIMARY KEY (name)
+);
+GO
+
+INSERT INTO cars ([name],model,hasPlates,plateNo,keyNo)
+VALUES ('Toyota Corolla','2008','True','WML755','1');
+
+
+select * FROM cars
