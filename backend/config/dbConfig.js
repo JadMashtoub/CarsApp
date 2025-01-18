0763eeda27de848524f9373e3f4d3b29767df157
Jad Mashtoub <jad.mash1@gmail.com>
2025-01-18 21:13:45 +1100

    view cars page done

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,7 +3,12 @@ const dbConfig = {
     password: process.env.DB_PASSWORD || 'fallback_password',
     server: process.env.DB_SERVER || 'fallback_server', 
     database: process.env.DB_DATABASE || 'fallback_database',
-   
+   pool: {
+    min: 1,
+    max: 10,
+    // IdleTimeoutMillis: 30000,
+   },
+  
    
     options: {
       encrypt: true,
